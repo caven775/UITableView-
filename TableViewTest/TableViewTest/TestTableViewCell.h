46cf00342bea9d2d9223647b19f057ad96d340c8
LKW <595282775@qq.com>
2017-01-14 14:41:13 +0800

    TestCell

---
+++ TableViewTest/TableViewTest/TestTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewCell.h
+//  TableViewTest
+//
+//  Created by 林克文 on 2017/1/14.
+//  Copyright © 2017年 LKW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewTest/TableViewTest/TestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.m
+//  TableViewTest
+//
+//  Created by 林克文 on 2017/1/14.
+//  Copyright © 2017年 LKW. All rights reserved.
+//
+
+#import "TestTableViewCell.h"
+
+@implementation TestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
